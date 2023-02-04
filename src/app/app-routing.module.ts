f06a49c72bc475e82573feaaa6b58caddf658b6f
KLaypexium <klaypexium@gmail.com>
2023-02-04 15:13:17 +0100

    categorys added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SoupsComponent } from './soups/soups.component';
+import { PoultryDishesComponent } from './poultry-dishes/poultry-dishes.component';
+import { FishFoodsComponent } from './fish-foods/fish-foods.component';
+import { MasterpiecesOfHungarianComponent } from './masterpieces-of-hungarian/masterpieces-of-hungarian.component';
+import { FriedFoodsComponent } from './fried-foods/fried-foods.component';
+import { MultipersonMealsComponent } from './multiperson-meals/multiperson-meals.component';
+import { SaladsComponent } from './salads/salads.component';
+import { MeatlessMealsComponent } from './meatless-meals/meatless-meals.component';
+import { GyrosComponent } from './gyros/gyros.component';
+import { AlcoholfreeDrinksComponent } from './alcoholfree-drinks/alcoholfree-drinks.component';
+import { AlcoholicDrinksComponent } from './alcoholic-drinks/alcoholic-drinks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SoupsComponent,
+    PoultryDishesComponent,
+    FishFoodsComponent,
+    MasterpiecesOfHungarianComponent,
+    FriedFoodsComponent,
+    MultipersonMealsComponent,
+    SaladsComponent,
+    MeatlessMealsComponent,
+    GyrosComponent,
+    AlcoholfreeDrinksComponent,
+    AlcoholicDrinksComponent
   ],
   imports: [
     BrowserModule,
